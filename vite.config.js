e6e9b56d9963f6dca213d1afd27f1694f048ab0f
Dadi Ishimwe <dadishimwe29@gmail.com>
2025-08-17 10:35:43 +0200

    Fix Tailwind CSS configuration for Vercel compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react()],
   base: '/datacents2/', // Add this for GitHub Pages
   resolve: {
     alias: {
